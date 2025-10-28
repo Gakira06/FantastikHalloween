3f777072917d41b59968c42a607ac85bad2c53df
Gabriel <gakirakiyohara@gmail.com>
2025-10-28 14:34:51 -0300

    chore: iniciando projeto

---
+++ frontend/src/pages/CatalogoPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CatalogoPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CatalogoPage
---
+++ frontend/src/pages/SobrePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SobrePage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SobrePage
