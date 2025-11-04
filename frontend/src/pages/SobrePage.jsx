9ad74c6345a54c5ea5618bb9239e46421a484b8b
Gustavo <gst.santos01@gmail.com>
2025-11-04 09:15:49 -0300

    fix: Ajustando rotas

--- frontend/src/pages/SobrePage.jsx
+++ frontend/src/pages/SobrePage.jsx
@@ -7,20 +7,22 @@ const SobrePage = () => {
     <div>
       <Header />
       <Navibar />
-      <div className="bg-black text-center p-4">
-        <h1 className="text-bold text-5xl py-9 text-white">Sobre Nós</h1>
-        <p className="text-white mx-auto max-w-2xl text-2xl">
+      <div className="bg-black text-center px-4 sm:px-8 py-6">
+        <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl py-4 text-white">
+          Sobre Nós
+        </h1>
+        <p className="text-white mx-auto max-w-2xl text-base sm:text-lg md:text-xl mb-4">
           Somos apaixonados por transformar momentos comuns em memórias
           extraordinárias. Na Fantastik, acreditamos que a fantasia certa tem o
           poder de libertar a imaginação e tornar qualquer celebração
           inesquecível.
         </p>
-        <p className="text-white p-8 mx-auto max-w-2xl text-2xl">
+        <p className="text-white mx-auto max-w-2xl text-base sm:text-lg md:text-xl mb-4 sm:mb-6 px-2 sm:px-8">
           Nossa missão é simples: oferecer "UMA GRANDE SELEÇÃO DE FANTASIAS".
           Embora nosso coração bata mais forte pelo Halloween, nossa coleção vai
           muito além, atendendo a todas as suas necessidades criativas.
         </p>
-        <p className="text-white mx-auto max-w-2xl text-2xl ">
+        <p className="text-white mx-auto max-w-2xl text-base sm:text-lg md:text-xl mb-2 sm:mb-4">
           Cada peça é selecionada pensando na qualidade e, claro, na diversão.
           Quer esteja a preparar-se para o Dia das Bruxas, uma festa temática ou
           apenas para despertar a sua criatividade, a Fantastik é o seu destino
